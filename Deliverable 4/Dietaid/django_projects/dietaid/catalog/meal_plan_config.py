4c70124a3f4f5e997d654a6c20f296f4a1fda3af
TheArnoldPickaxe <thearnoldpickaxe@gmail.com>
2023-04-20 23:32:07 +0400

    expanded diagnosis-diet mapping list

--- Deliverable 4/Dietaid/django_projects/dietaid/catalog/meal_plan_config.py
+++ Deliverable 4/Dietaid/django_projects/dietaid/catalog/meal_plan_config.py
@@ -26,6 +26,70 @@ MEALPLAN_MAPPER = {
         'breakfast': "Oatmeal (with added chia seeds, berries, or fruits of choice)",
         'lunch': "Grilled chicken or fish with cooked veggies (broccoli, carrot, beans, etc. can be added) or Any lentil curry with steamed rice or Oats atta roti",
         'dinner': "Mixed bean salad along with 1 cup of cooked quinoa or fish or a big bowl of seasonal vegetable cooked.",
-    },
-
+        },
+    'High blood pressure': {
+        'breakfast': "1 cup of fresh mixed fruits, 1 cup of fat-free, low-calorie vanilla-flavored yogurt, 1/3 cup walnuts, 1 bran muffin, 1 teaspoon trans-free margarine",
+        'lunch': "Curried chicken wrap w/ 2/3 cup cooked, chopped chicken, about 3 ounces; 1/2 cup, or about 8, raw baby carrots, and 1 cup fat-free milk",
+        'dinner': "1 cup cooked whole-wheat spaghetti with 1 cup marinara sauce, 2 cups mixed salad greens, 1 tablespoon low-fat Caesar dressing",
+        },
+    'PCOS': {
+        'breakfast': "Banana Yogurt Pots: 225g / ⅞ cup Greek yogurt, 225g / ⅞ cup Greek yogurt, 15g / 2 tbsp walnuts, toasted and chopped",
+        'lunch': "Cannellini Bean Salad, 600g / 3 cups cannellini beans, 70g / ⅜ cup cherry tomatoes, halved, ½ red onion, thinly sliced, ½ tbsp red wine vinegar, small bunch basil, torn",
+        'dinner': "	Quick Moussaka",
+        },
+    'Heart Health': {
+        'breakfast': "1 serving Avocado Egg Toast",
+        'lunch': "1 serving Loaded Black Bean Nacho Soup",
+        'dinner': "1 serving Seared Salmon with Green Peppercorn Sauce, 1 cup steamed green beans, 1 baked medium red potato, 1 Tbsp. nonfat plain Greek yogurt and a pinch of pepper.",
+        },
+    'Diabetes': {
+        'breakfast': "1 cup low-fat plain Greek yogurt, ¼ cup blueberries, 3 Tbsp. chopped walnuts",
+        'lunch': "1 serving White Bean & Veggie Salad",
+        'dinner': "1 serving Garlic Butter-Roasted Salmon with Potatoes & Asparagus",
+        },
+    'Hypothyroidism': {
+        'breakfast': "Cherry berry smoothie bowl",
+        'lunch': "Portable tuna pockets with fresh fruit",
+        'dinner': "Ultimate chopped salad",
+        },
+    'Overweight/obesity': {
+        'breakfast': "Banana Yogurt Pots: 225g / ⅞ cup Greek yogurt, 225g / ⅞ cup Greek yogurt, 15g / 2 tbsp walnuts, toasted and chopped",
+        'lunch': "Cannellini Bean Salad, 600g / 3 cups cannellini beans, 70g / ⅜ cup cherry tomatoes, halved, ½ red onion, thinly sliced, ½ tbsp red wine vinegar, small bunch basil, torn",
+        'dinner': "	Quick Moussaka",
+        },
+    'High cholesterol': {
+        'breakfast': "Banana Yogurt Pots: 225g / ⅞ cup Greek yogurt, 225g / ⅞ cup Greek yogurt, 15g / 2 tbsp walnuts, toasted and chopped",
+        'lunch': "Cannellini Bean Salad, 600g / 3 cups cannellini beans, 70g / ⅜ cup cherry tomatoes, halved, ½ red onion, thinly sliced, ½ tbsp red wine vinegar, small bunch basil, torn",
+        'dinner': "	Quick Moussaka",
+        },
+    'Hashimoto’s': {
+        'breakfast': "Cherry berry smoothie bowl",
+        'lunch': "Portable tuna pockets with fresh fruit",
+        'dinner': "Ultimate chopped salad",
+        },
+    'Menopause': {
+        'breakfast': "3-ingredient egg cups",
+        'lunch': "Roast beef with keto no-tato salad",
+        'dinner': "Baked chicken thighs with cauliflower rice and spinach dip",
+        },
+    'Diverticulitis': {
+        'breakfast': "White bread Potato sandwich (2) + Milk (Toned)",
+        'lunch': "Maida Roti (2) + Soy bean Curry (1/2 cup)",
+        'dinner': "Maida Roti (1) + Bottle Gourd Curry (1/2 cup)",
+        },
+    'Inflammation': {
+        'breakfast': "1 serving Blueberry-Banana Overnight Oats, and 1 cup green tea",
+        'lunch': "1 serving Green Salad with Edamame & Beets",
+        'dinner': "1 serving Walnut-Rosemary Crusted Salmon, 1 serving Roasted Squash & Apples with Dried Cherries & Pepitas",
+        },
+    'Arthritis': {
+        'breakfast': "1 serving Blueberry-Banana Overnight Oats, and 1 cup green tea",
+        'lunch': "1 serving Green Salad with Edamame & Beets",
+        'dinner': "1 serving Walnut-Rosemary Crusted Salmon, 1 serving Roasted Squash & Apples with Dried Cherries & Pepitas",
+        },
+    'GERD': {
+        'breakfast': "Low-fat yoghurt with melon and apple",
+        'lunch': "Turkey in a whole-grain wrap with spinach and low-fat hummus",
+        'dinner': "Poached salmon with brown quinio and courgette",
+        },
 }
